595d5038cdf416a32c15887f09167ba2a179ce1c
huzaifams <huzaifa.m.s@outlook.com>
2022-12-07 04:50:35 -0600

    Work on Template for Schools

--- buildTimeScripts/template.json.ts
+++ buildTimeScripts/template.json.ts
@@ -2,7 +2,8 @@ import { readFileSync } from 'fs';
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-import shayuks from './shayuk.json' assert { type: "json" }
+import shayuks from './data/shayuk.json' assert { type: "json" }
+import schools from './data/schools.json' assert { type: "json" }
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,6 +18,10 @@ const injectDataInTemplate = (template: string, data: any) => {
     let _template = template
     for (const attribute in data) {
         const regEx = new RegExp(`<!-- ${attribute} -->`, 'g')
+        console.log(regEx);
+        console.log(_template.match(regEx));
+        console.log(data[attribute]);
+
 
         _template = _template.replace(regEx, data[attribute])
     }
@@ -33,45 +38,46 @@ const injectDataInTemplate = (template: string, data: any) => {
 //     Ijazaat: string[]
 // }
 
-// const shaykUthman: Shayk = {
-//     picture: '/Ijazat-list/images/SheikUthmanIbnFarooq.webp',
-//     name: 'Shayk Uthman Ibn Farooq',
-//     bio: 'Bio',
-//     university: 'Islamic University',
-//     teacher: 'Teacher1',
-//     books: ['book1', 'book2'],
-//     Ijazaat: ['Ijazaat1', 'Ijazaat2']
-// }
-
-
 // Right now: Inject components & data -> send to templateMapping X
 // Make Data for Templates
 // Wire up for each page
 // Fetch data from bucket
 
 // Could gulp files -> Inject any components or data -> send to templateMapping
-const navbarComponent = getFileContents(resolve(__dirname, 'navbar.component.html'))
-const shaykTemplate = getFileContents(resolve(__dirname, 'shayk.component.html'))
+
+const navbarComponent = getFileContents(resolve(__dirname, './templates/navbar.component.html'))
+const shaykTemplate = getFileContents(resolve(__dirname, './templates/shayk.component.html'))
+const schoolTemplate = getFileContents(resolve(__dirname, './templates/school.component.html'))
 // Getting list of shayks to display
 
 const getAllShayuk = () => {
-    let shayksHtml = '<section class="shayuk-list">\n\r'
+    let shayksHtml = '<section class="shayuk-list">\n'
     for (const shayk in shayuks) {
-        const attr: any = shayuks[shayk as keyof typeof shayuks]
-        const shaykHtml = injectDataInTemplate(shaykTemplate, attr)
-        console.log(shayk);
+        const attrs: any = shayuks[shayk as keyof typeof shayuks]
+        const shaykHtml = injectDataInTemplate(shaykTemplate, attrs)
 
         shayksHtml += shaykHtml
-        shayksHtml += '\n\r'
+        shayksHtml += '\n'
     }
-    shayksHtml += '</section>\n\r'
+    shayksHtml += '</section>\n'
     return shayksHtml
 }
 
+const getAllSchools = () => {
+    let schoolsHtml = '<section class="shayuk-list">\n'
+    for (const school in schools) {
+        const attrs: any = schools[school as keyof typeof schools]
+        const schoolHtml = injectDataInTemplate(schoolTemplate, attrs)
 
+        schoolsHtml += schoolHtml
+        schoolsHtml += '\n'
+    }
+    schoolsHtml += '</section>\n'
+    return schoolsHtml
+}
 
 export const templateMapping = {
     '<!-- navbar -->': navbarComponent,
-    // '<!-- shaykUthman -->': shaykUthmanTemplate,
-    '<!-- allShayuk -->': getAllShayuk()
+    '<!-- allShayuk -->': getAllShayuk(),
+    '<!-- allSchools -->': getAllSchools(),
 }
